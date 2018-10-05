ad9607f2998850ed3aa84143eae9fa8d9b496862
domjos1994 <developing@domjos.de>
2018-10-05 17:29:55 +0200

    solve bug 00000070

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,23 +1,47 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'TimeLine',
-    'description' => 'An extension to create TimeLines in the FE!',
-    'category' => 'plugin',
-    'author' => 'Dominic Joas',
-    'author_company' => '',
-    'author_email' => 'entwicklung@domjos.de',
-    'state' => 'beta',
-    'clearCacheOnLoad' => true,
-    'version' => '0.0.2',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '7.6.0-8.9.99',
-        ]
-    ],
-    'autoload' => [
-        'psr-4' => [
-            'DominicJoas\\Timeline\\' => 'Classes'
-        ]
-    ],
-];
+/***************************************************************
+ * Extension Manager/Repository config file for ext "timeline".
+ *
+ * Auto generated 04-10-2018 15:57
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'TimeLine',
+  'description' => 'An extension to create TimeLines in the FE!',
+  'category' => 'plugin',
+  'author' => 'Dominic Joas',
+  'author_company' => '',
+  'author_email' => 'entwicklung@domjos.de',
+  'state' => 'beta',
+  'clearCacheOnLoad' => true,
+  'version' => '0.0.3',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-9.5.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'autoload' => 
+  array (
+    'psr-4' => 
+    array (
+      'DominicJoas\\Timeline\\' => 'Classes',
+    ),
+  ),
+  'uploadfolder' => false,
+  'createDirs' => NULL,
+  'clearcacheonload' => true,
+);
+
